c33fbca0a2871341b28b584ff2af8dae248f7518
Bomin Zhang <localvar@hotmail.com>
2024-07-13 11:48:50 +0800

    add about page

--- webui/src/views/AboutView.vue
+++ webui/src/views/AboutView.vue
@@ -1,6 +1,19 @@
+<template>
+	<a-typography>
+		<a-typography-title>MyIPTV</a-typography-title>
+		<a-typography-paragraph>
+			<ul>
+				<li>
+					<a-typography-text strong>项目地址：</a-typography-text> <a-typography-link href="https://github.com/localvar/myiptv">https://github.com/localvar/myiptv</a-typography-link>
+				</li>
+				<li>
+					<a-typography-text strong>作者：</a-typography-text> <a-typography-link href="https://blog.localvar.cn/">野生大老虎</a-typography-link>
+				</li>
+			</ul>
+			<a-typography-text> 欢迎提交 PR，本人不太擅长前端，所以尤其欢迎对前端进行优化和改进的 PR。 </a-typography-text>
+		</a-typography-paragraph>
+	</a-typography>
+</template>
+
 <script setup lang="ts">
 </script>
-
-<template>
-	<p>This is about</p>
-</template>
